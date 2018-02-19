450c61e67dd61ea0a4cf03fdb824fa6c4e944bb4
Munish Goyal <goyalmunish85@gmail.com>
2018-02-19 17:51:18 +0530

    module p5

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,19 @@
 console.log('Loaded!');
+
+
+var element = document.getElementById("main-text");
+
+element.innerHTML = "new value";
+
+
+var im = document.getElementById("madi");
+
+var marginLeft = 0;
+function marginRight(){
+	marginLeft = marginLeft + 10;
+	im.style.marginLeft = marginLeft +  "px";
+}
+im.onclick = function(){
+	var interval = setInterval(marginRight,100);
+	
+}
