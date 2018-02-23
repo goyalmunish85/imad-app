b93fd021ac2a70e39c677efb55e1bee3d45468ce
Munish Goyal <goyalmunish85@gmail.com>
2018-02-23 17:44:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,11 +24,25 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for(var i=0;i<names.length;i++){
-        list += '<li>' + names[i] + '</li>';
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.Done)
+        {
+            if(request.status === 200)
+            {
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0;i<names.length;i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('nameList');
+                ul.innerHTML = list;
+            }
+        }
     }
-    var ul = document.getElementById('nameList');
-    ul.innerHTML = list;
+    request.open('GET','http://goyalmunish85.imad.hasura-app.io/submit-name?name='+name,true);
+    
+    request.send(null);
+    
 }; 
