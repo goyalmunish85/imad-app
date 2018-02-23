ac41657a381434e1a9f924a50ff33a541c107814
Munish Goyal <goyalmunish85@gmail.com>
2018-02-23 17:21:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,11 +5,14 @@ var button = document.getElementById('counter');
     
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
-   
+        
+            if(request.status === 200)
+            {
                 var counter = request.responseText;
                 var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
-       
+            }
+        
     }
     request.open('GET','http://goyalmunish85.imad.hasura-app.io/counter',true);
     
